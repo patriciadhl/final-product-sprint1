d3428538efac27c6c701b0eeb70ddaeb65dacbe8
patriciadhl <patricia.dione@gmail.com>
2017-10-31 10:03:24 -0600

    Actualizando

--- is-valid-card/js/app.js
+++ is-valid-card/js/app.js
@@ -1,40 +1,32 @@
 var isValidCard=function(numCard){
-
-if(numCard ===""){
-  return alert('debes introducir numero de tarjeta');
-}else{
-var arr=[];
-
-for(a=0; a<numCard.length;a++){
- arr.push(parseInt(numCard.charAt(a))); //Creando arreglo con los datos de entrada
-}
-
-var newArr=arr.reverse(); //Revirtiendo valores del arreglo
-var sum=0;  //inicializando variable para realizar suma de los números
-
-for(i=0;i<newArr.length;i++){  //Recorrido de arreglo
-
-  if(i % 2 !==0){   //calculando módulo de dos para saber si la posición es par o impar
-    var numEvenPos=newArr[i] * 2; //multiplicando por 2 los números en posición par
-    var numString=numEvenPos.toString(); //convirtiendo a string resultado de la multipilcación
-
-    for(j=0;j<numString.length;j++){ //recorriendo numString
-     var strinInt=parseInt(numString.charAt(j)); //convirtiendo a entero para realizar la suma de los numeros obtenidos
-     sum=sum+strinInt; //suma de los numero obtenidos en posición par
-    }
+  if(numCard ===""){
+    return alert('debes introducir numero de tarjeta');
   }else{
-    sum=sum+newArr[i]; //suma de los numero obtenidos en posición par mas números en posición impar
+    var arr=[];
+    for(a=0; a<numCard.length;a++){
+      arr.push(parseInt(numCard.charAt(a))); //Creando arreglo con los datos de entrada
+    }
+    var newArr=arr.reverse(); //Revirtiendo valores del arreglo
+    var sum=0;  //inicializando variable para realizar suma de los números
+    for(i=0;i<newArr.length;i++){  //Recorrido de arreglo
+      if(i % 2 !==0){   //calculando módulo de dos para saber si la posición es par o impar
+        var numEvenPos=newArr[i] * 2; //multiplicando por 2 los números en posición par
+        var numString=numEvenPos.toString(); //convirtiendo a string resultado de la multipilcación
+        for(j=0;j<numString.length;j++){ //recorriendo numString
+          var strinInt=parseInt(numString.charAt(j)); //convirtiendo a entero para realizar la suma de los numeros obtenidos
+          sum=sum+strinInt; //suma de los numero obtenidos en posición par
+        }
+      }else{
+        sum=sum+newArr[i]; //suma de los numero obtenidos en posición par mas números en posición impar
+      }
+    }
+    if (sum % 10 === 0){  //verificando si la tarjeta es válida o invalida
+      return document.write('Tarjeta válida');
+    }else{
+      return document.write('Tarjeta invalida');
+    }
   }
 }
-
-if (sum % 10 === 0){  //verificando si la tarjeta es válida o invalida
-  return document.write('Tarjeta válida');
-}else{
-  return document.write('Tarjeta invalida');
-}
-}
-}
-
 
 var numCard=prompt("INGRESA EL NUMERO DE TARJETA A VALIDAR");
 isValidCard(numCard);
